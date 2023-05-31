c87bc85fcfe7aff86e20b7caa9e29f5be6d2a4bd
Levi Schaut <schaut.levi@gmail.com>
2023-05-31 09:21:13 -0500

    Add end win section
    Adds a brief section after the player collects all scientists where they have to escape back to the exit.  When they do, they are greeted with a (unfinished) win screen.

--- Assets/Scripts/ScientistIndicator.cs
+++ Assets/Scripts/ScientistIndicator.cs
@@ -8,17 +8,25 @@ public class ScientistIndicator : MonoBehaviour
 {
     public TMP_Text indicatorText;
     public float number;
+    public TMP_Text returnToExitText;
+    public GameObject winZone;
 
     // Start is called before the first frame update
     void Start()
     {
         number = 3f;
         indicatorText.text = "Scientists Left to Rescue: " + number.ToString();
+        returnToExitText.gameObject.SetActive(false);
+        winZone.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         indicatorText.text = "Scientists Left to Rescue: " + number.ToString();
+        if (number < 1) {
+            returnToExitText.gameObject.SetActive(true);
+            winZone.SetActive(true);
+        }
     }
 }
